67ac2caad7ddd03e360882ba6c6bdfc9f9798eaa
cliffg-softwarelibre <cliffg-softwarelibre@users.noreply.github.com>
2025-04-02 21:20:55 +0000

    deploy: ec6201c6faa656fc6d7ed5f1ee6693a16c4f375c

--- search/pages_1.js
+++ search/pages_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['mpmc_20thread_20safe_20queue_0',['Wait Queue, a Multi-Writer / Multi-Reader (MPMC) Thread-Safe Queue',['../index.html',1,'']]],
-  ['multi_20writer_20multi_20reader_20mpmc_20thread_20safe_20queue_1',['Wait Queue, a Multi-Writer / Multi-Reader (MPMC) Thread-Safe Queue',['../index.html',1,'']]]
+  ['multi_20reader_20mpmc_20thread_20safe_20queue_1',['Wait Queue, a Multi-Writer / Multi-Reader (MPMC) Thread-Safe Queue',['../index.html',1,'']]],
+  ['multi_20writer_20multi_20reader_20mpmc_20thread_20safe_20queue_2',['Wait Queue, a Multi-Writer / Multi-Reader (MPMC) Thread-Safe Queue',['../index.html',1,'']]]
 ];
--- search/pages_2.js
+++ search/pages_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['queue_20a_20multi_20writer_20multi_20reader_20mpmc_20thread_20safe_20queue_0',['Wait Queue, a Multi-Writer / Multi-Reader (MPMC) Thread-Safe Queue',['../index.html',1,'']]]
+  ['queue_0',['Wait Queue, a Multi-Writer / Multi-Reader (MPMC) Thread-Safe Queue',['../index.html',1,'']]],
+  ['queue_20a_20multi_20writer_20multi_20reader_20mpmc_20thread_20safe_20queue_1',['Wait Queue, a Multi-Writer / Multi-Reader (MPMC) Thread-Safe Queue',['../index.html',1,'']]]
 ];
